67ea2046c455dcf1b05107b859daadebd6fbc366
Aayush Gupta <guptaaayush432@gmail.com>
2024-01-24 12:18:13 +0530

    Added mint() function

--- src/Token.sol
+++ src/Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.7;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
@@ -10,5 +10,9 @@ contract Token is ERC20 {
         uint256 initialSupply
     ) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
     }
 }
